d475c6d298dd06091158be02936f613636fecb7a
goqiewd <cheari1018@gmail.com>
2025-11-04 21:55:22 +0900

    class 내의 필요 함수들 추가

--- bird.py
+++ bird.py
@@ -6,6 +6,12 @@ import game_framework
 class bird:
     image = None
 
-    def __init__(self):
+    def __init__(self,x=100,y=300):
         if bird.image == None:
             bird.image = load_image('bird_animation.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
