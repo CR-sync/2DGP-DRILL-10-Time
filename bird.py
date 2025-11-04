5f380463906c1e3e5c3082783aef46d2cd7e26ae
goqiewd <cheari1018@gmail.com>
2025-11-04 22:04:29 +0900

    bird의 초기 기본 값 설정
    x, y 자리, 왼 오 화면 밖으로 나가지 않게끔 최소 최대 범위

--- bird.py
+++ bird.py
@@ -3,12 +3,18 @@ import game_world
 import game_framework
 
 
-class bird:
+
+class Bird:
     image = None
 
-    def __init__(self,x=100,y=300):
-        if bird.image == None:
-            bird.image = load_image('bird_animation.png')
+    def __init__(self,x=100,y=300,left=50, right=750):
+        if self.image == None:
+            self.image = load_image('bird_animation.png')
+
+        self.x, self.y = x, y
+        self.frame = 0
+        self.face_dir = 1
+        self.dir = 0
 
     def update(self):
         pass
