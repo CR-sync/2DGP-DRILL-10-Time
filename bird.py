adc6bce02d8f2dd56d1de627f19a4db79c66a684
goqiewd <cheari1018@gmail.com>
2025-11-04 22:08:22 +0900

    bird frame 시간 계산

--- bird.py
+++ bird.py
@@ -3,6 +3,14 @@ import game_world
 import game_framework
 
 
+# bird의 Run Speed 계산
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_MPS = 6.0 #6 m/s
+RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
+# bird Run Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 14
 
 class Bird:
     image = None
